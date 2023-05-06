e59ed4f2580416429e248ccf0cb125613fbb3a48
elitesuper <tonylobbin7712@gmail.com>
2023-05-06 05:11:14 -0700

    update table

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, user_login
+from .views import index, user_login, get_data
 
 app_name = "app"
 
 urlpatterns = [
     path("", index, name="index"),
     path("login/", user_login, name="login"),
+    path("get-data/", get_data, name='getData')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from django.db import connections
+from django.http import JsonResponse
 import json
 
 # Create your views here.
@@ -31,3 +33,31 @@ def user_login(request):
             return render(request, 'login.html', {'error': 'Invalid username or password'})
     else:
         return render(request, 'login.html')
+
+@csrf_exempt
+def get_data(request):
+    if request.method == 'POST':
+        draw = int(request.POST.get('draw'), 0)
+        start = int(request.POST.get('start'), 0)
+        length = int(request.POST.get('length'), 10)
+    with connections['trackdb'].cursor() as cursor:
+        # Modify the query to use the start and length parameters
+        cursor.execute("SELECT * FROM track_table LIMIT %s, %s", [start, length])
+        rows = cursor.fetchall()
+        cursor.execute("SELECT COUNT (*) AS cnt FROM track_table")
+        data = cursor.fetchall()
+        total_records = data[0][0]
+
+    
+    data = [{
+        'id' : row[0],
+        'status' : row[5],
+        'time' : row[3]
+    } for row in rows]
+
+    return JsonResponse ({
+        'draw':draw,
+        'recordsTotal': int(total_records),
+        'recordsFiltered' : int(total_records),
+        'data':data,
+    }, safe=False)

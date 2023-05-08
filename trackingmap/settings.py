cc546a7baf8eb210d4d89d5f5fead5de32158db0
elitesuper <tonylobbin7712@gmail.com>
2023-05-08 05:21:14 -0700

    localhost added

--- trackingmap/settings.py
+++ trackingmap/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(_3dj=&owl2zwjzczl-x%=-gvq#-su3tp!x#=wit^vtrjuh+5#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["149.28.180.176"]
+ALLOWED_HOSTS = ["149.28.180.176","localhost"]
 
 
 # Application definition
